a3a419b330458f5438335204695f0c54502486f3
Centrix14 <artiom.zvyagin@yandex.ru>
2020-09-26 19:22:33 +0300

    header for draw.c

---
+++ draw.h
@@ -0,0 +1,13 @@
+#ifndef __FLAT_DRAW_DRAW_LIBRARY_HEADER_FILE_INCLUDED__
+#define __FLAT_DRAW_DRAW_LIBRARY_HEADER_FILE_INCLUDED__
+
+#include "figure.h"
+#include "data/list.h"
+
+void dl_set_cairo_context(cairo_t *cr);
+void dl_draw_figure(figure *fptr);
+void dl_draw_figure_list(list *lptr);
+void dl_draw_point(figure *fptr);
+void dl_draw_line_pp(figure *fptr);
+
+#endif
