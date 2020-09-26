979dba0d78190a4608c2ab9fdf54bda19c6ec15e
Centrix14 <artiom.zvyagin@yandex.ru>
2020-09-26 19:23:04 +0300

    new functions

--- figure.h
+++ figure.h
@@ -1,19 +1,30 @@
 #ifndef __FLAT_DRAW_FIGURE_LIBRARY_HEADER_FILE_INCLUDED__
 #define __FLAT_DRAW_FIGURE_LIBRARY_HEADER_FILE_INCLUDED__
 
+#include "data/list.h"
+
 typedef struct {
 	int x;
 	int y;
 
-	int w;
-	int h;
+	int a1;
+	int a2;
 
 	int type;
+
+	int visible;
 } figure;
 
 enum FG_TYPES {
 	FG_TYPE_POINT = 0,
-	FG_TYPE_LINE
+	FG_TYPE_LINE_PP
 };
+
+figure *figure_new(int type, int x, int y, int a1, int a2);
+figure *figure_new_point(int x, int y);
+figure *figure_new_line_pp(int x1, int y1, int x2, int y2);
+
+void figure_free(figure *fptr);
+void figure_free_list(list *lptr);
 
 #endif
