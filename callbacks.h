4b2854ec5c229e4900a6ef0eaa869d64ab9cdc1f
Centrix14 <artiom.zvyagin@yandex.ru>
2020-09-26 19:19:30 +0300

    added new callbacks functions

--- callbacks.h
+++ callbacks.h
@@ -5,4 +5,7 @@ gboolean draw_area_draw(GtkWidget *area, cairo_t *cr, gpointer data);
 gboolean mouse_move(GtkWidget *widget, GdkEvent *event, gpointer data);
 gboolean mouse_click(GtkWidget *widget, GdkEvent *event, gpointer data);
 
+void point_bttn_click(GtkWidget *bttn, gpointer data);
+void line_bttn_click(GtkWidget *bttn, gpointer data);
+
 #endif
