d46b5591445105ca964025032f89f0431e0944aa
Centrix14 <artiom.zvyagin@yandex.ru>
2020-09-27 14:45:26 +0300

    added preview mode

--- draw.h
+++ draw.h
@@ -4,10 +4,13 @@
 #include "figure.h"
 #include "data/list.h"
 
-void dl_set_cairo_context(cairo_t *cr);
 void dl_draw_figure(figure *fptr);
 void dl_draw_figure_list(list *lptr);
+
 void dl_draw_point(figure *fptr);
 void dl_draw_line_pp(figure *fptr);
+
+void dl_set_cairo_context(cairo_t *cr);
+void dl_set_preview_coords(int px, int py);
 
 #endif
