48522f84883d8cefd90013ed49d10476ac59968f
Centrix14 <artiom.zvyagin@yandex.ru>
2020-10-01 18:22:41 +0300

    added convertation from grades to rads

--- geometry.h
+++ geometry.h
@@ -3,6 +3,8 @@
 
 #include "figure.h"
 
+#define PI 3.14159265359
+
 double gel_calculate_lenght(figure *line);
 
 void gel_calculate_intersection(figure *line1, figure *line2, figure *point);
@@ -11,6 +13,8 @@ double gel_lf_calculate_k(figure *line);
 double gel_lf_calculate_b(figure *line);
 int gel_lf_is_parallel(figure *line1, figure *line2);
 
-void gel_calculate_point(figure *point, double lenght, double angle);
+void gel_calculate_line_la(figure *point, double lenght, double angle);
+
+double gel_convert_grades_to_rads(double grades);
 
 #endif
