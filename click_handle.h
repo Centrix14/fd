01f42753dd6ddd9d7b46428868e3aad96ac1615e
Centrix14 <artiom.zvyagin@yandex.ru>
2020-10-01 18:21:57 +0300

    added creation by lenght and angle

--- click_handle.h
+++ click_handle.h
@@ -3,10 +3,12 @@
 
 void ch_set_draw_mode(int new_mode);
 void ch_set_parent_window(GtkWidget *window);
+void ch_set_external_figure(figure *fptr);
 
 void ch_click_handler(GtkWidget *draw_area, list *lptr, int x, int y);
 
 void ch_add_point(GtkWidget *draw_area, list *lptr, int x, int y);
 void ch_add_line_pp(GtkWidget *draw_area, list *lptr, int x, int y);
+void ch_add_line_la(GtkWidget *draw_area, list *lptr, int x, int y);
 
 #endif
