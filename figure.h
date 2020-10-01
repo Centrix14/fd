cf4a258e7c5aff4f5d35d503e5432540d47b873c
Centrix14 <artiom.zvyagin@yandex.ru>
2020-10-01 19:26:38 +0300

    added rect & rect_decompose function

--- figure.h
+++ figure.h
@@ -18,7 +18,8 @@ typedef struct {
 enum FG_TYPES {
 	FG_TYPE_POINT = 0,
 	FG_TYPE_LINE_PP,
-	FG_TYPE_LINE_LA
+	FG_TYPE_LINE_LA,
+	FG_TYPE_RECT_PP
 };
 
 enum VISIBLE_MODES {
@@ -30,8 +31,11 @@ enum VISIBLE_MODES {
 figure *figure_new(int type, int x, int y, int a1, int a2);
 figure *figure_new_point(int x, int y);
 figure *figure_new_line_pp(int x1, int y1, int x2, int y2);
+figure *figure_new_rect_pp(int x, int y, int w, int h);
 
 void figure_free(figure *fptr);
 void figure_free_list(list *lptr);
+
+figure *figure_rect_decompose(figure *rect);
 
 #endif
