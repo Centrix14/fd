9eda3c664f8a9e3905a18cbe9a3794ba8825a064
Centrix14 <artiom.zvyagin@yandex.ru>
2020-10-03 12:08:25 +0300

    added new preview model, more safety

--- draw.h
+++ draw.h
@@ -17,5 +17,10 @@ void dl_set_preview_coords(int px, int py);
 int dl_is_need_draw(figure *fptr);
 
 void dl_switch_display_all_lays();
+void dl_switch_show_preview();
+
+void dl_send_preview_figure(figure *fptr);
+
+void dl_draw_preview();
 
 #endif
