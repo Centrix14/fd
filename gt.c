f14f15a9cf32fc8c2c96cc10b24a3f9792e7ff34
Centrix14 <artiom.zvyagin@yandex.ru>
2020-10-04 17:04:35 +0300

    removed gt.c

--- gt.c
+++ gt.c
@@ -4,13 +4,15 @@
 #include "geometry.h"
 
 int main() {
-	figure *l1;
+	figure *l1, *l2, *p;
 
-	l1 = figure_new_line_pp(5, 5, 0, 0);
+	l1 = figure_new_line_pp(0, 0, 5, 0);
+	l2 = figure_new_line_pp(5, 5, 5, 0);
+	
+	p = figure_new_point(0, 0);
 
-	gel_calculate_point(l1, 5, 45);
+	gel_calculate_intersection(l1, l2, p);
 
-	printf("%f %f\n", l1->a1, l1->a2);
-
+	printf("p %f %f\n", p->x, p->y);
 	return 0;
 }
