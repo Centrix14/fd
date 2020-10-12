65f156e65160abb5df8d85c753abfd39807a4b42
Centrix14 <artiom.zvyagin@yandex.ru>
2020-10-12 18:57:31 +0300

    added translation betwwen colorschemes

--- color.h
+++ color.h
@@ -4,12 +4,14 @@
 #include <gtk/gtk.h>
 
 void cl_set_color(cairo_t *cr, int color_set);
+void cl_set_color_fg(cairo_t *cr, int fg_color_set);
 
 enum DEF_COLORS {
 	CL_DEF_CURS_COLOR = 0,
 	CL_DEF_DRAW_COLOR,
 	CL_DEF_PREVIEW_COLOR,
-	CL_DEF_PROJECTION_COLOR
+	CL_DEF_PROJECTION_COLOR,
+	CL_DEF_SELECTED_COLOR
 };
 
 #endif
