835bf65da80ade0060ff3ad040bb41186e993aab
Centrix14 <artiom.zvyagin@yandex.ru>
2020-10-26 19:08:01 +0300

    found center

--- geometry.h
+++ geometry.h
@@ -20,15 +20,17 @@ double gel_convert_rads_to_grades(double rads);
 
 figure *gel_get_middle_point(figure *line);
 double gel_get_line_angle(figure *l);
+double gel_get_max_lenght(figure *a, figure *b, figure *c);
+figure gel_get_triangle_center(figure *a, figure *b, figure *c);
+figure *gel_get_middle_point(figure *line);
+figure *gel_get_equal_point(figure *line1, figure *line2);
+figure *gel_get_center_point_by_hr(double xh, double yh, double R);
+figure *gel_get_arc_center(figure *l1, figure *l2);
 
 int gel_is_middle_point_area(figure *line, double x, double y, int area);
 int gel_is_point_in_line(figure *l, figure *p);
 int gel_is_point_in_rect(figure *r, figure *p);
 int gel_is_point_in_circle(figure *c, figure *p);
 int gel_is_point_in_point(figure *p1, figure *p2);
-
-figure *gel_get_middle_point(figure *line);
-figure *gel_get_equal_point(figure *line1, figure *line2);
-figure *gel_get_center_point_by_hr(double xh, double yh, double R);
 
 #endif
