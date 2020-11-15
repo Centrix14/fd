dd4660892d7edcebfcdee4c194d47694aaf78990
Centrix14 <artiom.zvyagin@yandex.ru>
2020-11-15 19:44:43 +0300

    removed needless functions

--- click_handle.h
+++ click_handle.h
@@ -2,7 +2,6 @@
 #define __FLAT_DRAW_CLICK_HANDLE_LIBRARY_HEADER_FILE_INCLUDED__
 
 void ch_set_draw_mode(int new_mode);
-void ch_set_parent_window(GtkWidget *window);
 void ch_set_external_figure(figure *fptr);
 void ch_set_state(int new_state);
 
