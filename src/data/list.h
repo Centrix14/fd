d3ccaf101ed1abbe8359cea3484b204c66d30062
Centrix14 <artiom.zvyagin@yandex.ru>
2020-11-29 15:31:02 +0300

    added blocks

--- src/data/list.h
+++ src/data/list.h
@@ -8,14 +8,16 @@ typedef struct __list__ {
 } list;
 
 list *list_init_node(list *parent);
-void list_free_node(list *node);
 void list_add_node(list *parent);
-void list_free_list(list *node);
 void list_set_data(list *node, void *new_data);
 void list_show(list *root);
-list *list_get_last(list *root);
 void list_crawl(list *root, void (*func)(list*));
-void *list_get_data(list *lptr);
 void list_dump_node(list *lptr);
+
+void list_free_node(list *node);
+void list_free_list(list *node);
+
+list *list_get_last(list *root);
+void *list_get_data(list *lptr);
 
 #endif
