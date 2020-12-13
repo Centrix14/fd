d806cd8ee99dd5832a6d1de7a17359fc35264048
Centrix14 <artiom.zvyagin@yandex.ru>
2020-12-13 12:16:48 +0300

    changed manual link

--- src/help.c
+++ src/help.c
@@ -3,7 +3,7 @@
 static GtkWidget *help_label = NULL;
 
 static char *help_msg[] = {
-		"Welcome to FlatDraw!\nThis program allows you to create drawings and sketches.\nTo get help, go to the website: https://github.com/Centrix14/fd/help.md\nDocumentation is located at this address: https://github.com/Centrix14/fd/blob/master/doc/fd_doc.md\nFLatDraw v0.15 by Centrix 13.12.2020",
+		"Welcome to FlatDraw!\nThis program allows you to create drawings and sketches.\nTo get help, go to the website: https://github.com/Centrix14/fd/blob/master/doc/user_manual.md\nDocumentation is located at this address: https://github.com/Centrix14/fd/blob/master/doc/fd_doc.md\nFLatDraw v0.15 by Centrix14 13.12.2020",
 		"Select point",
 		"Select start point",
 		"Select end point",
