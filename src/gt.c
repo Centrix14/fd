be51d42dc93401a239cb80d12292e36eb7a340f6
Centrix14 <artiom.zvyagin@yandex.ru>
2020-12-18 19:16:37 +0300

    tmp test file

---
+++ src/gt.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#include "geometry.h"
+#include "figure.h"
+
+int main() {
+	figure *l1, *l2;
+
+	l1 = figure_new_line_pp(0, 0, -5, -5);
+
+	printf("%s: %g\n", __func__, gel_calculate_line_angle(l1));
+	return 0;
+}
