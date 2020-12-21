c97502d1f40898406e5b0d4d410a4ebc5cb7b38b
Centrix14 <artiom.zvyagin@yandex.ru>
2020-12-21 19:16:07 +0300

    continue debug

--- src/gt.c
+++ src/gt.c
@@ -5,9 +5,11 @@
 
 int main() {
 	figure *l1;
+	double a = 315;
 
-	l1 = figure_new_line_pp(0, 0, -5, -5);
+	l1 = figure_new_line_pp(0, 0, 0, 0);
+	gel_calculate_line_la(l1, 5, a);
 
-	printf("%s: %g\n", __func__, gel_calculate_line_angle(l1));
+	printf("%s: %g %g\t%g %g\n", __func__, l1->x, l1->y, l1->a1, l1->a2);
 	return 0;
 }
