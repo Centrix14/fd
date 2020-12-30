8e9bbf7b2194852887b9e26f5c490ec62b2459ee
Centrix14 <artiom.zvyagin@yandex.ru>
2020-12-30 19:05:23 +0300

    draw function

--- src/multi_obj.c
+++ src/multi_obj.c
@@ -1,6 +1,9 @@
+#include <gtk/gtk.h>
+
 #include "data/list.h"
 #include "figure.h"
 #include "text.h"
+#include "draw.h"
 
 void mol_free_from_node(list *lptr) {
 	text *tptr;
@@ -19,6 +22,22 @@ void mol_free_from_node(list *lptr) {
 			if (tptr->buffer)
 				tl_free_buffer(tptr);
 			tl_free(tptr);
+		break;
+	}
+}
+
+void mol_draw_obj_from_node(list *lptr) {
+	if (!lptr || !lptr->data)
+		return ;
+
+	switch(lptr->dt) {
+		case OT_FIGURE:
+			dl_draw_figure((figure*)lptr->data);
+		break;
+
+		case OT_TEXT:
+			if (((text*)(lptr->data))->visible == VM_SHOW)
+				dl_draw_text((text*)lptr->data);
 		break;
 	}
 }
