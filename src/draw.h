8c456b51eda6f75cc7aa7124fbeaf37175344776
Centrix14 <artiom.zvyagin@yandex.ru>
2020-12-30 19:04:43 +0300

    added text

--- src/draw.h
+++ src/draw.h
@@ -3,9 +3,11 @@
 
 #include "figure.h"
 #include "data/list.h"
+#include "text.h"
 
 void dl_draw_figure(figure *fptr);
 void dl_draw_figure_list(list *lptr);
+void dl_draw_text(text *tptr);
 
 void dl_draw_point(figure *fptr);
 void dl_draw_line_pp(figure *fptr);
