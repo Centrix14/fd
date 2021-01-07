32c512bdf8e58cb79061012003611f2c161d6152
Centrix14 <artiom.zvyagin@yandex.ru>
2021-01-07 16:22:18 +0300

    i must add tests

--- src/dbg.h
+++ src/dbg.h
@@ -2,5 +2,6 @@
 #define __DBG_SPEC_HAEDER_FILE_FOR_FLAT_DRAW_CAD__
 
 //#define DBG
+#define text_log(ptr) {fprintf(stderr, "%s: tptr = <%p> font = %s <%p> size = %d rgb = %d %d %d\n", __func__, ptr, font, font, size, r, g, b);}
 
 #endif
