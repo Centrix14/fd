a24ecb9022ff4fd00dc274280cf966391626bf42
Centrix14 <artiom.zvyagin@yandex.ru>
2021-01-13 17:33:34 +0300

    changed the project tree

--- src/multi_obj.h
+++
@@ -1,7 +0,0 @@
-#ifndef __FLAT_DRAW_MULTI_OBJECT_PROCESSING_LIBRARY_HEADER_FILE_INCLUDED__
-#define __FLAT_DRAW_MULTI_OBJECT_PROCESSING_LIBRARY_HEADER_FILE_INCLUDED__
-
-void mol_free_from_node(list *lptr);
-void mol_draw_obj_from_node(list *lptr);
-
-#endif
