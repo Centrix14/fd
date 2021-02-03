4e3c8df51512e19aeafc9928c20c8da69c2fdcf1
Centrix14 <artiom.zvyagin@yandex.ru>
2021-02-03 19:06:22 +0300

    start to add text

--- src/fd_format/fd_format.h
+++ src/fd_format/fd_format.h
@@ -1,14 +1,17 @@
 #ifndef __FLAT_DRAW_FORMAT_PROCESSING_LIBRARY_HEADER_FILE_INCLUDED__
 #define __FLAT_DRAW_FORMAT_PROCESSING_LIBRARY_HEADER_FILE_INCLUDED__
 
-#define FD_FORMAT_DELIM " \n"
+#define FD_FORMAT_END "/end\n"
 
 void fdl_target_file(char *name);
 
-void fdl_write_figure_stream(FILE *stream, figure *fptr);
+void fdl_write_object_stream(FILE *stream, list *lptr);
 void fdl_write_from_list(list *lptr);
 
 void fdl_read_string(figure *fptr, char *str);
 void fdl_read_file(list *lptr);
+
+void fdl_pars_figure(FILE *stream, list *lptr);
+void fdl_pars_text(FILE *stream, list *lptr);
 
 #endif
