3490ef40c33785056b1f40964318fbe720882812
Centrix14 <artiom.zvyagin@yandex.ru>
2021-02-17 19:17:21 +0300

    remove

--- src/dbg.h
+++
@@ -1,7 +0,0 @@
-#ifndef __DBG_SPEC_HAEDER_FILE_FOR_FLAT_DRAW_CAD__
-#define __DBG_SPEC_HAEDER_FILE_FOR_FLAT_DRAW_CAD__
-
-//#define DBG
-#define text_log(ptr) {fprintf(stderr, "%s: tptr = <%p> font = %s <%p> size = %d rgb = %d %d %d\n", __func__, ptr, font, font, size, r, g, b);}
-
-#endif
