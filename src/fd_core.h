b3178a44b6258f3df5ebf99aea3b94fcba5cb7d3
Centrix14 <artiom.zvyagin@yandex.ru>
2021-03-23 15:42:48 +0300

    common fd libary

---
+++ src/fd_core.h
@@ -0,0 +1,26 @@
+#ifndef __FLAT_DRAW_COMMON_LIBRARY_FLAT_DRAW_CORE_HEADERS_HEADER_FILE_INCLUDED__
+#define __FLAT_DRAW_COMMON_LIBRARY_FLAT_DRAW_CORE_HEADERS_HEADER_FILE_INCLUDED__
+
+#include <gtk/gtk.h>
+
+// fd core
+#include "binding/binding.h"
+#include "color/color.h"
+#include "draw/draw.h"
+#include "error/error.h"
+#include "fd_format/fd_format.h"
+#include "figure/figure.h"
+#include "geometry/geometry.h"
+#include "help/help.h"
+#include "list/list.h"
+#include "multi_obj/multi_obj.h"
+#include "plugin/plugin.h"
+#include "so/so.h"
+#include "text/text.h"
+#include "util/util.h"
+
+// additional utils
+#include "pechkin/pl.h"
+#include "st.h/st.h"
+
+#endif
