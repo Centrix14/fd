034ca81206a9e7d28bf14525ca8044387d23c1a8
Centrix14 <artiom.zvyagin@yandex.ru>
2021-03-23 15:41:17 +0300

    new plugin load libary

---
+++ src/plugin/plugin.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <gtk/gtk.h>
+
+#include "../util/util.h"
+#include "../so/so.h"
+#include "../list/list.h"
+#include "plugin.h"
+
+#include "../st.h/st.h"
+
+void pil_load_plugins(GtkWidget *drawing_area, list *figures) {
+	char *plug_list_home = NULL, *plug_list_current = NULL;
+
+	plug_list_current = ".fdplug";
+	plug_list_home = ul_get_file_from_home(plug_list_current);
+	if (ul_is_exist(plug_list_home))
+		pil_load_list(plug_list_home, drawing_area, figures);
+	else if (ul_is_exist(plug_list_current))
+		pil_load_list(plug_list_current, drawing_area, figures);
+}
+
+void pil_load_list(char *filename, GtkWidget *drawing_area, list *figures) {
+	FILE *plug_list = NULL;
+	char str[256] = "";
+
+	plug_list = fopen(filename, "r");
+	fgets(str, 256, plug_list);
+	while (!feof(plug_list)) {
+		pil_load_plugin(str, drawing_area, figures);
+		fgets(str, 256, plug_list);
+	}
+	
+	fclose(plug_list);
+}
+
+void pil_load_plugin(char *filename, GtkWidget *drawing_area, list *figures) {
+	void (*plug_fn)(GtkWidget*, list*) = NULL;
+	char *correct_filename = NULL;
+
+	correct_filename = ul_remove_char(filename, '\n'); // fgets returns a string with \n
+	sl_open_file(correct_filename);
+
+	plug_fn = sl_get_function("plug_main");
+	(*plug_fn)(drawing_area, figures);
+}
