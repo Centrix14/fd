716c858a724d751059ee07c8b5b972e4fd58aabe
Centrix14 <artiom.zvyagin@yandex.ru>
2021-04-12 18:48:59 +0300

    options library

---
+++ src/options/opt.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../list/list.h"
+#include "opt.h"
+
+#include "../st.h/st.h"
+
+void ol_check_options(list *node, int opt_code, void *opt_val) {
+	if (node && !node->opt)
+		ol_add_options(node);
+}
+
+void ol_add_options(list *node) {
+	options *new_opt = NULL;
+
+	new_opt = malloc(sizeof(options));
+	if (!new_opt) {
+		st_err("fail to allocate options field");
+	}
+
+	new_opt->r = 0;
+	new_opt->g = 0;
+	new_opt->b = 0;
+
+	new_opt->tag = NULL;
+	new_opt->group = NULL;
+
+	node->opt = (void*)new_opt;
+}
+
+void ol_free_node(list *node) {
+	if (node && node->opt)
+		free(node->opt);
+}
