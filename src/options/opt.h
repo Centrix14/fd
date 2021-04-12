b407427407c25ebefc79c992c2a7271144c0a660
Centrix14 <artiom.zvyagin@yandex.ru>
2021-04-12 18:49:02 +0300

    options library

---
+++ src/options/opt.h
@@ -0,0 +1,19 @@
+#ifndef __FLAT_DRAW_CAD_MULTI_OBJECT_OPTIONS_LIBRARY_HEADER_FILE_INCLUDED__
+#define __FLAT_DRAW_CAD_MULTI_OBJECT_OPTIONS_LIBRARY_HEADER_FILE_INCLUDED__
+
+#include "../list/list.h"
+
+typedef struct {
+	int r;
+	int g;
+	int b;
+
+	char *group;
+	char *tag;
+} options;
+
+void ol_check_options(list *node, int opt_code, void *opt_val);
+void ol_add_options(list *node);
+void ol_free_node(list *node);
+
+#endif
