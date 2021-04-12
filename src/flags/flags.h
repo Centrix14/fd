e9dd9c4fb983eb828780ac030a62f4b8f83e3d6f
Centrix14 <artiom.zvyagin@yandex.ru>
2021-04-12 18:03:44 +0300

    new library: flags

---
+++ src/flags/flags.h
@@ -0,0 +1,15 @@
+#ifndef __FLAT_DRAW_CAD_FLAGS_PERSER_LIBRARY_HEADER_FILE_INCLUDED__
+#define __FLAT_DRAW_CAD_FLAGS_PERSER_LIBRARY_HEADER_FILE_INCLUDED__
+
+void fl_parse(int argc, char *argv[]);
+void fl_parse_letter(int lett);
+void fl_parse_str(char *str);
+
+void fl_flag_q();
+void fl_flag_h();
+void fl_flag_v();
+
+void fl_flag_plug_off();
+void fl_flag_links();
+
+#endif
