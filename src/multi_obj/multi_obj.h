1fc111db8c938034a91bb3343b160a61c21019ec
Centrix14 <artiom.zvyagin@yandex.ru>
2021-05-17 19:28:28 +0300

    fight with bugs

--- src/multi_obj/multi_obj.h
+++ src/multi_obj/multi_obj.h
@@ -13,6 +13,7 @@ typedef struct {
 void mol_free_from_node(list *lptr);
 void mol_draw_obj_from_node(list *lptr);
 figure *mol_conv_to_figure(list *lptr);
+void mol_context_color_set(list *lptr);
 
 multi_obj *mol_extract(list *lptr);
 multi_obj *mol_extract_from_text(list *lptr);
