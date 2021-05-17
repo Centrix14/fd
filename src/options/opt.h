e240ca8f2830331e01f49a731fd9d9b1efa0998a
Centrix14 <artiom.zvyagin@yandex.ru>
2021-05-17 19:07:39 +0300

    expand options

--- src/options/opt.h
+++ src/options/opt.h
@@ -16,4 +16,7 @@ void ol_check_options(list *node);
 void ol_add_options(list *node);
 void ol_free_node(list *node);
 
+options *ol_get_opt(list *node);
+void ol_set_color(list *node, double r, double g, double b);
+
 #endif
