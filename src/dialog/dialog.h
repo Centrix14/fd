7b0a070f358ef14157ccb34d47af752ba83cdb06
Centrix14 <artiom.zvyagin@yandex.ru>
2021-09-20 19:01:07 +0300

    new lib

---
+++ src/dialog/dialog.h
@@ -0,0 +1,11 @@
+#ifndef __FLAT_DRAW_DIALOG_LIBRARY_HEADER_FILE_INCLUDED__
+#define __FLAT_DRAW_DIALOG_LIBRARY_HEADER_FILE_INCLUDED__
+
+typedef enum {
+	DCF_SAVE = 0,
+	DCF_OPEN
+} DIAL_CHOOSE_FUNC;
+
+void dial_show_file_choose_dialog(DIAL_CHOOSE_FUNC dcf);
+
+#endif
