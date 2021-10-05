4bb0e5fe155afefd99b47093fc4fa007b0f6b68f
Centrix14 <artiom.zvyagin@yandex.ru>
2021-10-05 19:15:08 +0300

    work in progress

--- src/dialog/dialog.h
+++ src/dialog/dialog.h
@@ -8,7 +8,13 @@ typedef enum {
 
 void dial_show_file_choose_dialog(DIAL_CHOOSE_FUNC dcf);
 
-void __dial_fill_dir_list(GtkWidget *list_box);
+// service
+void __dial_fill_dir_list(GtkWidget *list_box, char *path);
+void __dial_remove_widget_glib_func(GtkWidget *element, GtkWidget *list_box);
+void __dial_list_box_clear(GtkWidget *list_box);
+
+// callbacks
 void __dial_act_bttn_click(GtkWidget *bttn, gpointer data);
+void __dial_go_by_addr_bttn_click(GtkWidget *bttn, gpointer data);
 
 #endif
