d0614f1b58cf6910be9ec3fb8b6ebf0679b36925
Centrix14 <artiom.zvyagin@yandex.ru>
2021-10-30 21:03:51 +0300

    plugin system update

--- src/plugin/plugin.c
+++ src/plugin/plugin.c
@@ -9,7 +9,7 @@
 #include "../st.h/st.h"
 #include "../pechkin/pl.h"
 
-void pil_load_plugins(GtkWidget *drawing_area, list *figures) {
+void pil_load_plugins(list *msg_list) {
 	char *plug_list_home = NULL, *plug_list_current = NULL;
 
 	if (pl_read("msg:plug_off")) // if plug-ins switched off
@@ -18,32 +18,34 @@ void pil_load_plugins(GtkWidget *drawing_area, list *figures) {
 	plug_list_current = ".fdplug";
 	plug_list_home = ul_get_file_from_home(plug_list_current);
 	if (ul_is_exist(plug_list_home))
-		pil_load_list(plug_list_home, drawing_area, figures);
+		pil_load_list(plug_list_home, msg_list);
 	else if (ul_is_exist(plug_list_current))
-		pil_load_list(plug_list_current, drawing_area, figures);
+		pil_load_list(plug_list_current, msg_list);
 }
 
-void pil_load_list(char *filename, GtkWidget *drawing_area, list *figures) {
+void pil_load_list(char *filename, list *msg_list) {
 	FILE *plug_list = NULL;
 	char str[256] = "";
 
 	plug_list = fopen(filename, "r");
 	fgets(str, 256, plug_list);
 	while (!feof(plug_list)) {
-		pil_load_plugin(str, drawing_area, figures);
+		pil_load_plugin(str, msg_list);
 		fgets(str, 256, plug_list);
 	}
 	
 	fclose(plug_list);
 }
 
-void pil_load_plugin(char *filename, GtkWidget *drawing_area, list *figures) {
-	void (*plug_fn)(GtkWidget*, list*) = NULL;
+void pil_load_plugin(char *filename, list *msg_list) {
+	void (*plug_fn)(list*) = NULL;
 	char *correct_filename = NULL;
 
 	correct_filename = ul_remove_char(filename, '\n'); // fgets returns a string with \n
 	sl_open_file(correct_filename);
 
 	plug_fn = sl_get_function("plug_main");
-	(*plug_fn)(drawing_area, figures);
+	if (plug_fn)
+		(*plug_fn)(msg_list);
+	sl_close();
 }
