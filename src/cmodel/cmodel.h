20b850eb3ddc1b3f465e0938eeb95867f7506a86
Centrix14 <artiom.zvyagin@yandex.ru>
2022-01-16 18:01:59 +0300

    new functions

--- src/cmodel/cmodel.h
+++ src/cmodel/cmodel.h
@@ -6,16 +6,18 @@ void init(list *msg_list);
 void model();
 
 // core functions
-void point(double x, double y);
-void line_xy(double x1, double y1, double x2, double y2);
-void line_ar(double x, double y, double angle, double r);
-void rect_xy(double x1, double y1, double x2, double y2);
-void rect_wh(double x1, double y1, double w, double h);
-void circle(double x, double y, double r);
-void label(double crd[2], int rgb[3], unsigned int size, char *font, char *str);
+figure *point(double x, double y);
+figure *line_xy(double x1, double y1, double x2, double y2);
+figure *line_ar(double x, double y, double angle, double r);
+figure *rect_xy(double x1, double y1, double x2, double y2);
+figure *rect_wh(double x1, double y1, double w, double h);
+figure *circle(double x, double y, double r);
+text *label(double crd[2], int rgb[3], unsigned int size, char *font, char *str);
 
 // util functions
 void info();
+void erase(void *ptr, OBJ_TYPE ot);
+char *get_param(char *msg);
 
 // service functions
 void set_msg_list(list *msg_list);
